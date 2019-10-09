0a30640e473040a6d5744dd26190ecc277228357
Stef Spears <spears.sj87@gmail.com>
2019-10-09 05:35:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word  
+  end 
+  
+  def match(arr)
+    match_word = []
+    arr.each do |w| 
+      if @word.split("").sort == w.split("").sort
+      match_word << w 
+      end
+    end
+    match_word
+   # binding.pry
+  end
+
+ 
+end 
